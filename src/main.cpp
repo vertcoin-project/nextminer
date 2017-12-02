d44002a51942a90d9984c46a74f15f2c59612ab2
James Lovejoy <jameslovejoy1@gmail.com>
2017-12-01 23:56:48 -0500

    Implement worker interupts on new work issuance

--- src/main.cpp
+++ src/main.cpp
@@ -8,44 +8,56 @@ int main() {
     std::unique_ptr<NextMiner::GetWork> workSource(
         new NextMiner::StratumClient("18.250.0.71", 9271, "jamesl222", "x", log.get()));
 
+    // Wait until the workSource has work for us
     auto work = workSource->getWork();
     while(!work) {
         work = workSource->getWork();
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
-    const uint256 target = CompactToTarget(work->getTarget());
-    uint256 lowest("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+    bool stale = false;
+    workSource->registerWorker([&](const bool discardOld){
+        stale = true;
+    });
 
-    log->printf("Trying to get: " + target.ToString(),
-                NextMiner::Log::Severity::Notice);
+    bool running = true;
+    while(running) {
+        work = workSource->getWork();
+        stale = false;
+        const uint256 target = CompactToTarget(work->getTarget());
+        uint256 lowest("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
 
-    std::vector<uint8_t> outputBuffer;
-    outputBuffer.resize(32);
+        log->printf("Trying to get: " + target.ToString(),
+                    NextMiner::Log::Severity::Notice);
 
-    uint32_t nonce = 0;
+        std::vector<uint8_t> outputBuffer;
+        outputBuffer.resize(32);
 
-    while(true) {
-        work->setNonce(nonce++);
-        const auto headerBytes = work->getBytes();
+        uint32_t nonce = 0;
 
-        lyra2re2_hash(reinterpret_cast<const char*>(&headerBytes[0]),
-                      reinterpret_cast<char*>(&outputBuffer[0]));
+        while(!stale) {
+            work->setNonce(nonce++);
+            const auto headerBytes = work->getBytes();
 
-        const uint256 result = CBigNum(outputBuffer).getuint256();
+            lyra2re2_hash(reinterpret_cast<const char*>(&headerBytes[0]),
+                          reinterpret_cast<char*>(&outputBuffer[0]));
 
-        if(nonce % (1000) == 0) {
-            log->printf(lowest.ToString(), NextMiner::Log::Severity::Notice);
-        }
+            const uint256 result = CBigNum(outputBuffer).getuint256();
 
-        if(result < target) {
-            const auto res = workSource->submitWork(*work);
-            log->printf("Found valid share!! " + std::get<1>(res), NextMiner::Log::Severity::Notice);
-            break;
-        }
+            if(nonce % (1000) == 0) {
+                log->printf(lowest.ToString(), NextMiner::Log::Severity::Notice);
+            }
 
-        if(result < lowest) {
-            lowest = result;
+            if(result < target) {
+                const auto res = workSource->submitWork(*work);
+                log->printf("Found valid share!! " + std::get<1>(res), NextMiner::Log::Severity::Notice);
+                running = false;
+                break;
+            }
+
+            if(result < lowest) {
+                lowest = result;
+            }
         }
     }
 
