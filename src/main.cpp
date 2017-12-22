c7b52477340e6c51a13e3c6ce5ed0b3b35a46b6f
James Lovejoy <jameslovejoy1@gmail.com>
2017-12-22 16:55:08 -0500

    Finish WorkProxy implementation

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,29 @@
 #include "util.h"
 #include "stratumclient.h"
 #include "cpumanager.h"
+#include "workproxy.h"
 
 int main() {
     std::unique_ptr<NextMiner::Log> log(new NextMiner::Log);
-    std::unique_ptr<NextMiner::GetWork> workSource(
-        new NextMiner::StratumClient("18.250.5.90", 9171,
+    std::unique_ptr<NextMiner::GetWork> workSource1(
+        new NextMiner::StratumClient("18.250.0.71", 9271,
+                                     "VnUxCjoLSv1U5pxegJBGxX8anDnop77DWz", "x",
+                                     log.get()));
+
+    std::unique_ptr<NextMiner::GetWork> workSource2(
+        new NextMiner::StratumClient("198.204.233.158", 9171,
                                      "VnUxCjoLSv1U5pxegJBGxX8anDnop77DWz", "x",
                                      log.get()));
 
     // Wait until the workSource has work for us
-    while(!workSource->getWork()) {
+    while(!workSource2->getWork() || !workSource1->getWork()) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+
+    std::unique_ptr<NextMiner::WorkProxy> workProxy(
+        new NextMiner::WorkProxy(workSource1.get(), 10));
+
+    workProxy->addWorkSource(workSource2.get(), 10);
 
     #ifdef __linux
     if(setpriority(PRIO_PROCESS, 0, -15) == -1) {
@@ -36,7 +47,8 @@ int main() {
     }
     #endif // __linux
 
-    std::unique_ptr<NextMiner::CPUManager> CPU(new NextMiner::CPUManager(workSource.get(), log.get()));
+    std::unique_ptr<NextMiner::CPUManager> CPU(
+        new NextMiner::CPUManager(workProxy.get(), log.get()));
 
     std::this_thread::sleep_for(std::chrono::seconds(120));
 
